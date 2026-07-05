d5723fa072eaebcfedc1f5c34d9722fa47a86c8e
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2026-07-05 13:44:08 +0300

    style(test): normalize quote style and formatting in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'json-summary'],
+      reporter: ["text", "json-summary"],
       reportOnFailure: true,
-    }
-  }
+    },
+  },
 });
